6585173c4ebfe8b8fe0a997beaec9619c797cccb
Gerald <geraldsang1550@gmail.com>
2026-01-15 15:05:18 +0300

    mode-commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "./components/theme-provider";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
@@ -25,7 +23,6 @@ const App = () => (
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
-  </ThemeProvider>
 );
 
 export default App;
