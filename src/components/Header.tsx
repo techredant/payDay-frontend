df2026cf0433b96583f6eb0d0f612982013c770e
Gerald <geraldsang1550@gmail.com>
2026-01-18 23:14:29 +0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -84,7 +84,7 @@ useEffect(() => {
           )}
 
           {/* Admin button */}
-          {currentUser && profiles.some((p) => p.email === "abrahamngetich907@gmail.com" && p.email === currentUser.email) && (
+          {currentUser && profiles.some((p) => p.email === "techredant@gmail.com" && p.email === currentUser.email) && (
             <Link to="/admin">
               <Button variant="ghost" size="sm">
                 Add tip
